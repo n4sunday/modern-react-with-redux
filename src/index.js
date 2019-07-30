a3c1db51978928e53e5e6be40039c128ff786602
n4s <s5935512004@phuket.psu.ac.th>
2019-07-30 11:33:21 +0700

    Building Content with JSX

--- src/index.js
+++ src/index.js
@@ -1,8 +1,59 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import faker from 'faker'
+import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
-    return <div>Hello World</div>
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:45PM"
+                    content="Nice blog post"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Ajex"
+                    timeAgo="Today at 1:00PM"
+                    content="I link the subject"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+
+                    author="Jane"
+                    timeAgo="Testerday at 5:25PM"
+                    content="Nice blog post"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Today at 2:13AM"
+                    content="I link the writing"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+
+        </div>
+    )
 }
 
-ReactDOM.render(<App />,document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'))
