554cbefe3f6d6c2748a245b12c4e39c17c8b8e80
n4s <s5935512004@phuket.psu.ac.th>
2019-07-30 22:32:43 +0700

    Determining Season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
-const SesonDisplay = () => {
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter'
+    }
+    else {
+        return lat > 0 ? 'winter' : 'summer'
+    }
+}
+
+const SesonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth())
+
+    console.log(season)
+     
     return <div>SeasonDisplay</div>
 }
 
--- src/index.js
+++ src/index.js
@@ -3,31 +3,26 @@ import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { lat: null, errorMessage: '' }
-    }
-    render() {
+    state = { lat: null, errorMessage: '' }
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                console.log(position)
-                // we called setstate
-                this.setState({ lat: position.coords.latitude })
-
-
-                // we did not
-                this.state.lat = position.coords.latitude
-            },
-            err => {
-                this.setState({ errorMessage: err.message })
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         )
+    }
+    componentDidUpdate() {
+        console.log('My component was just updated - it rerender')
+    }
+
+    render() {
+
 
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
         return (
             <div>Loading...</div>
