5746365bb189028d601203cfdb4c2d993749ed56
n4s <s5935512004@phuket.psu.ac.th>
2019-08-05 02:47:22 +0700

    Install Axios and Viewing Request Results

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
 
 class App extends Component {
     onSearchSubmit(term) {
-        console.log(term)
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 04691650ad21cfb0bd3d77f19854daac2d609b07e1a65d5c3ef5234cf8a31918'
+            }
+        })
     }
 
     render() {
