e748ed4daa496dec9ffa9f171801a35d56a68223
n4s <s5935512004@phuket.psu.ac.th>
2019-08-09 02:33:04 +0700

    Rendering Lists Components

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const ImageList = props => {
     console.log(props.images)
+    const images = props.images.map((image) => {
+        return <img src={image.urls.regular} />
+    })
     return (
-        <div></div>
+        <div>{images}</div>
     )
 }
 
