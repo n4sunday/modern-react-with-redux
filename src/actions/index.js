7cda5feb134c73dc6d6373289c63cb2ffeb052a4
Sunday <s5935512004@phuket.psu.ac.th>
2019-10-15 08:01:46 +0700

    Update UI Redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 // Action creator
-export const selectSong = () => {
+export const selectSong = song => {
     // Return an action
     return {
         type: 'SONG_SELECTED',
         payload: song
     }
 }
+
 
